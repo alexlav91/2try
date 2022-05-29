f1c0cdb1c874a57bf15dceccb8ef98dee4189f58
alexlav91 <105648254+alexlav91@users.noreply.github.com>
2022-05-29 17:39:39 +0400

    Create User.java

---
+++ src/MyList/User.java
@@ -0,0 +1,60 @@
+package MyList;
+
+public class User implements Comparable <User> {
+
+    private String name;
+    private int age;
+    private int height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public User() {
+
+    }
+
+
+
+    public User(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    @Override
+    public int compareTo(User u) {
+        if(u.height > this.height){
+            System.out.println(u.name + "Выше чем " + this.name);
+            return 1;
+        }else if(u.height < this.height){
+            System.out.println(u.name + "Ниже чем " + this.name);
+            return -1;
+        }else{
+            System.out.println(u.name + " Эквивалентен " + this.name);
+            return 0;
+        }
+
+    }
+
+}
